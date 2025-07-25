b7e3328eafda60909781b92cf42643db190484a8
Isaac Oliveira Loiol <isaackopi@icloud.com>
2025-07-25 08:57:17 -0300

    Adicionar arquivos de projeto.

---
+++ BankTests/BanckAccountTests.cs
@@ -0,0 +1,26 @@
+﻿
+using Bank;
+
+namespace BankTests
+{
+    [TestClass]
+    public sealed class BanckAccountTests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            //Arange
+            double saldoInicial = 11.99;
+            double quantidaDebito = 4.55;
+            double expectativa = 7.44;
+            BankAccount conta = new("Isaac Oliveira Loiola", saldoInicial);
+
+            //Act
+            conta.Debit(quantidaDebito);
+
+            //Assert
+            double atual = conta.Balance;
+            Assert.AreEqual(expectativa, atual, 0.001, "Conta não debitada corretamente");
+        }
+    }
+}
